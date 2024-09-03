7facbf95bbef440894dea9af1a5bc62b531a37e3
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-04 02:41:27 +0600

    practice vue

---
+++ first_project_vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to My First Vue Project" />
+    <HomePage />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import HomePage from './components/HomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HomePage
+  }
+}
+</script>
