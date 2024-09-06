c5f1f3495a8b9f3c6a188437b67f8e91cf0c5f2f
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-06 18:46:19 +0600

    vue practice

--- first_project/src/App.vue
+++ first_project/src/App.vue
@@ -1,30 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script> 
+export default{ 
+  data () { 
+    return{ 
+      name: 'John Cina',
+      status false,
+    };
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{name}}</h1>
+  <p v-if="status">user is active</p>
+  <p v-else>User is Unactive</p>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+
