ec177337548a1aeb57f5f5ee2f440c1fe0c4e836
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-06 20:07:33 +0600

    practice vue

--- vue-practice/src/components/HomePage.vue
+++ vue-practice/src/components/HomePage.vue
@@ -1,18 +1,29 @@
 <template>
-    <h3>This is the Home Page</h3>
+    <h3>Email: {{ email }}</h3>
+    <h3>Phone: {{ phone }}</h3>
+    <h3>Name: {{ getName() }}</h3> <!-- getName() ফাংশন হিসেবে কল করা হয়েছে -->
 </template>
-
 
 <script>
 export default {
     name: 'HomePage',
     data (){ 
-        return{ 
+        return { 
             email: 'tuhin@gmail.com',
-
+            phone: '0175587876'
+        }
+    },
+    methods: {  // methods আলাদা সেকশনে রাখা হয়েছে
+        getName() {
+            return "Tuhin Ahmed";
         }
     }
-    
 }
 </script>
 
+
+<style scoped>
+h3{ 
+    color: green;
+}
+</style>
