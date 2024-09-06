865e82c3f76b07d6262ff8aceee03bdb25c6b18d
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-06 18:56:35 +0600

    practice vue

---
+++ vue-practice/src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Home Page</h1>
+</template>
+
+<script> 
+
+export default { 
+    name: 'HomePage'
+}
+</script>
+
+<style scoped>
+h1{ 
+    color: green;
+    text-align: center;
+    margin: 50px;
+}
+</style>
