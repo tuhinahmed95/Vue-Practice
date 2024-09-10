c292c86cfbba4b397c956577179e21554ed05b64
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-10 21:02:34 +0600

    practice vue route

--- route-vue/src/router/index.js
+++ route-vue/src/router/index.js
@@ -1,23 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+// define route component by importing
+import HomeView from "@/views/HomeView.vue";
+import AboutView from "@/views/AboutView.vue";
+import ContactView from "@/viewss/ContactView.vue";
+import PostView from "@/viewss/PostView.vue";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
-
-export default router
+// define routes,Each route should map to a component
