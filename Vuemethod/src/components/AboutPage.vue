7c8456142c73e14f4e56314d791bd3d07bfe72d6
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-15 22:23:46 +0600

    method

---
+++ Vuemethod/src/components/AboutPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>{{ email }}</h1>
+</template>
+
+<script>
+export default{ 
+    name: 'AboutPage',
+    data(){ 
+        return{ 
+            email: 'mtuhinahmded@gmail.com'
+        }
+    }
+}
+</script>
