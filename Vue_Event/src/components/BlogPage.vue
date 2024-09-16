3c677324a9853121e6bc6989c0d77b6708aa7d09
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-16 18:56:03 +0600

    methods with params

--- Vue_Event/src/components/BlogPage.vue
+++ Vue_Event/src/components/BlogPage.vue
@@ -1,10 +1,27 @@
 <template>
-    <h3>Blog Page</h3>
+    <h1>Blog Page</h1>
+    <h2>Blog : {{ blog }}</h2>
+    <h3>blog : {{ getBlog() }}</h3>
+    <h4>Blog2 : {{ getBlog2('getblog 3') }}</h4>
+
 </template>
 
 <script> 
 export default { 
-    name: 'BlogPage'
+    name: 'BlogPage',
+    data(){ 
+        return{
+            blog: 'this blog is practice'
+        }
+    },
+    methods:{ 
+        getBlog(){ 
+            return 'this is a blog methods'
+        },
+        getBlog2(blog2){ 
+            return blog2;
+        }
+    }
 }
 </script>
 
--- Vue_Event/src/components/HomePage.vue
+++ Vue_Event/src/components/HomePage.vue
@@ -1,10 +1,25 @@
 <template>
     <h1>This Is Home Page</h1>
+    <h2>Email : {{ email }},</h2>
+    <h3>Phone Number : {{ phone ==8888}}</h3>
+    <h3>Phone Number : {{ phone}}</h3>
+    <h4>Full Name : {{ getName ('tuhin ahmed')}}</h4>
 </template>
 
 <script> 
 export default{ 
+    
     name: 'HomePage',
+    data(){ 
+        return{ 
+            email:'tuhin@test.com',
+            phone:'9999',
+            getName:function(t){ 
+                return t;
+            }
+        }
+        
+    }
    
 }
 
@@ -14,4 +29,5 @@ export default{
 h1{ 
     color: burlywood;
 }
+
 </style>
