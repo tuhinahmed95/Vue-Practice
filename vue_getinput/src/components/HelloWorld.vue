683b559904a51322c05d37f7a22cfb78b5a482e0
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-17 12:28:13 +0600

    vue

---
+++ vue_getinput/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Home Page</h1>
+  <input type="text" placeholder="enter your email"><br><br>
+  <input type="text" placeholder="enter your password"><br><br>
+  <button type="button" v-on:click="getData">Get Values</button>
+</template>
+
+<script> 
+export default { 
+  name:'HelloWorld',
+  methods:{ 
+    getData(){ 
+      console.log("function called");
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{ 
+  color: green;
+}
+</style>
