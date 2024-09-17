fa051e2a950dc715d90143478d858b1917b03ca1
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-09-17 15:37:52 +0600

    vue loop

--- vue_loop_datapass/src/App.vue
+++ vue_loop_datapass/src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <HomeComponents />
-  <ForLoop />
+<HomeComponents />
+<ForLoop />
+<ChildComponet name="Thin Ahmed" :details="user" :setData="getData" />
 </template>
 
 <script>
+import ChildComponet from './components/ChildComponet.vue';
 import ForLoop from './components/ForLoop.vue';
 import HomeComponents from './components/HomeComponents.vue';
 
-export default { 
-  name:'App',
-  components:{ 
-    HomeComponents,
-    ForLoop
-  }
+export default {
+    name: 'App',
+    components: {
+        HomeComponents,
+        ForLoop,
+        ChildComponet
+    },
+    data() {
+        return {
+            user: {
+                name: 'Alex',
+                email: 'alex@gmail.com'
+            },
+        },
+        methods: {
+            getData() {
+              colsole.log('function is called');
+               
+            }
+        }
+    }
 }
 </script>
---
+++ vue_loop_datapass/src/components/ChildComponet.vue
@@ -0,0 +1,23 @@
+<template>
+<h1>Data pass From ParentComponent to ChildComponent</h1>
+<h3>{{ name }}</h3>
+<h4>{{ user.name }}</h4>
+<button v-on:click="getData()">Call F</button>
+</template>
+
+<script>
+ export default {
+     name: 'ChildComponent',
+     props: {
+         name: String,
+         user: Object,
+         getData: function
+     }
+ }
+</script>
+
+<style scoped>
+h1 {
+    color: cadetblue;
+}
+</style>
